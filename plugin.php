6f8be7f8f3e169ddab48eba113b910b9e0463ff7
theodortruffer <tt@studer-raimann.ch>
2016-12-16 14:00:52 +0100

    support for ilias 5.2

--- plugin.php
+++ plugin.php
@@ -1,7 +1,7 @@
 <?php
 $id = 'usrto';
-$version = '1.1.4';
+$version = '1.1.5';
 $ilias_min_version = '4.3.0';
-$ilias_max_version = '5.1.999';
+$ilias_max_version = '5.2.999';
 $responsible = 'Martin Studer, Fabian Schmid';
 $responsible_mail = 'info@studer-raimann.ch';
