59a10e02ea4be669ecffeb4997b81370bdf27f3c
Oskar Truffer <ot@studer-raimann.ch>
2017-02-23 16:36:19 +0100

    higher plugin version

--- plugin.php
+++ plugin.php
@@ -2,6 +2,6 @@
 $id = 'usrto';
 $version = '1.1.4';
 $ilias_min_version = '4.3.0';
-$ilias_max_version = '5.1.999';
+$ilias_max_version = '5.2.999';
 $responsible = 'Martin Studer, Fabian Schmid';
 $responsible_mail = 'info@studer-raimann.ch';
