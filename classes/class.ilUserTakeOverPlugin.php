e2488eb52ee129bb9f084b59e8e74c654ba43b17
Oskar Truffer <ot@studer-raimann.ch>
2017-03-03 16:01:20 +0100

    fixed doubled leave user view entry

--- classes/class.ilUserTakeOverPlugin.php
+++ classes/class.ilUserTakeOverPlugin.php
@@ -34,32 +34,4 @@ class ilUserTakeOverPlugin extends ilUserInterfaceHookPlugin {
 	public function getPluginName() {
 		return 'UserTakeOver';
 	}
-
-
-	/**
-	 * @param int $id
-	 *
-	 * @return ctrlmmEntryCtrl[]
-	 */
-	public static function getMenuEntries($id = 0) {
-		if (!$_SESSION[usrtoHelper::USR_ID_BACKUP]) {
-			return array();
-		}
-
-		$entries[$id] = array();
-		$entries[0] = array();
-		if (is_file('./Customizing/global/plugins/Services/UIComponent/UserInterfaceHook/CtrlMainMenu/classes/EntryTypes/Link/class.ctrlmmEntryLink.php')) {
-			$ilUserTakeOverPlugin = ilUserTakeOverPlugin::getInstance();
-			$hub_menu = new ctrlmmEntryLink();
-			$hub_menu->setUrl('goto.php?target=usr_takeback');
-			$hub_menu->setTitle($ilUserTakeOverPlugin->txt("leave_user_view"));
-			$hub_menu->setPermissionType(ctrlmmMenu::PERM_NONE);
-			$hub_menu->setTarget('');
-			$hub_menu->setPlugin(true);
-
-			$entries[0][] = $hub_menu;
-		}
-
-		return $entries[$id];
-	}
 }
