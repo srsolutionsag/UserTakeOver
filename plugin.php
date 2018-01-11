580fe432f0d146b326b3cbf33a66440f4896092d
Florian <fw@studer-raimann.ch>
2018-01-11 10:39:59 +0100

    Remove old ILIAS support

--- plugin.php
+++ plugin.php
@@ -1,7 +1,7 @@
 <?php
 $id = 'usrto';
 $version = '1.1.10';
-$ilias_min_version = '4.3.0';
+$ilias_min_version = '5.2.0';
 $ilias_max_version = '5.3.999';
 $responsible = 'Martin Studer, Fabian Schmid';
 $responsible_mail = 'info@studer-raimann.ch';
