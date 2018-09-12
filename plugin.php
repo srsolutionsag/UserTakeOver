93ea61a03b192c4ca69b63a81686463a551b332f
Benjamin Seglias <bs@studer-raimann.ch>
2018-09-12 10:01:46 +0200

    adopted responsible

--- plugin.php
+++ plugin.php
@@ -3,5 +3,5 @@ $id = 'usrto';
 $version = '1.2.11';
 $ilias_min_version = '5.2.0';
 $ilias_max_version = '5.3.999';
-$responsible = 'Team Core 1';
+$responsible = 'studer + raimann ag - Team Core 1';
 $responsible_mail = 'support-core1@studer-raimann.ch';
