536487b34e438e9b1b0546f3d8eacbc37c3e399e
Florian <fw@studer-raimann.ch>
2018-11-07 08:36:17 +0100

    Supports latest libraries

--- classes/class.ilUserTakeOverConfig.php
+++ classes/class.ilUserTakeOverConfig.php
@@ -11,28 +11,10 @@ class ilUserTakeOverConfig extends ActiveRecordConfig {
 
 	const TABLE_NAME = 'ui_uihk_usrto_config_n';
 	const PLUGIN_CLASS_NAME = ilUserTakeOverPlugin::class;
-
 	/**
-	 * @return bool|null
+	 * @var array
 	 */
-	public static function getUninstallRemovesData()/*: ?bool*/ {
-		return self::getXValue(ilUserTakeOverRemoveDataConfirm::KEY_UNINSTALL_REMOVES_DATA, ilUserTakeOverRemoveDataConfirm::DEFAULT_UNINSTALL_REMOVES_DATA);
-	}
+	protected static $fields = [
 
-
-	/**
-	 * @param bool $uninstall_removes_data
-	 */
-	public static function setUninstallRemovesData(/*bool*/$uninstall_removes_data)/*: void*/ {
-		self::setBooleanValue(ilUserTakeOverRemoveDataConfirm::KEY_UNINSTALL_REMOVES_DATA, $uninstall_removes_data);
-	}
-
-	/**
-	 *
-	 */
-	public static function removeUninstallRemovesData()/*: void*/ {
-		self::removeName(ilUserTakeOverRemoveDataConfirm::KEY_UNINSTALL_REMOVES_DATA);
-	}
-
-
+	];
 }
--- classes/uninstall/class.ilUserTakeOverRemoveDataConfirm.php
+++ classes/uninstall/class.ilUserTakeOverRemoveDataConfirm.php
@@ -7,34 +7,10 @@ use srag\RemovePluginDataConfirm\AbstractRemovePluginDataConfirm;
 /**
  * Class ilUserTakeOverRemoveDataConfirm
  *
- * @author: Benjamin Seglias   <bs@studer-raimann.ch>
+ * @author            : Benjamin Seglias   <bs@studer-raimann.ch>
  * @ilCtrl_isCalledBy ilUserTakeOverRemoveDataConfirm: ilUIPluginRouterGUI
  */
-
 class ilUserTakeOverRemoveDataConfirm extends AbstractRemovePluginDataConfirm {
 
 	const PLUGIN_CLASS_NAME = ilUserTakeOverPlugin::class;
-
-	/**
-	 * @inheritdoc
-	 */
-	public function getUninstallRemovesData()/*: ?bool*/ {
-		return ilUserTakeOverConfig::getUninstallRemovesData();
-	}
-
-
-	/**
-	 * @inheritdoc
-	 */
-	public function setUninstallRemovesData(/*bool*/$uninstall_removes_data)/*: void*/ {
-		ilUserTakeOverConfig::setUninstallRemovesData($uninstall_removes_data);
-	}
-
-
-	/**
-	 * @inheritdoc
-	 */
-	public function removeUninstallRemovesData()/*: void*/ {
-		ilUserTakeOverConfig::removeUninstallRemovesData();
-	}
 }
