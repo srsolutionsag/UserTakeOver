1c04b828b649b4358d9556f5cd8fb58f54444f53
Florian Wyss <fw@studer-raimann.ch>
2018-11-29 12:13:53 +0100

    Fix team

--- plugin.php
+++ plugin.php
@@ -3,5 +3,5 @@ $id = 'usrto';
 $version = '1.2.12';
 $ilias_min_version = '5.2.0';
 $ilias_max_version = '5.3.999';
-$responsible = 'studer + raimann ag - Team Core 1';
-$responsible_mail = 'support-core1@studer-raimann.ch';
+$responsible = 'studer + raimann ag - Team Core';
+$responsible_mail = 'support-core@studer-raimann.ch';
