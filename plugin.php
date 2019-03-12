5b0d8d04f7a1ec348256304b4d6a3892881c0b3e
Florian Wyss <fw@studer-raimann.ch>
2019-03-12 07:54:50 +0100

    ILIAS 5.4 base version

--- plugin.php
+++ plugin.php
@@ -2,6 +2,6 @@
 $id = 'usrto';
 $version = '2.0.0';
 $ilias_min_version = '5.2.0';
-$ilias_max_version = '5.3.999';
+$ilias_max_version = '5.4.999';
 $responsible = 'studer + raimann ag - Team Core';
 $responsible_mail = 'support-core@studer-raimann.ch';
