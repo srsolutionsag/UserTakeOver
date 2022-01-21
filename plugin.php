81ed35caeac0454452c6cbd7eb054ed12fccd0a8
Fabian Schmid <fabian@sr.solutions>
2022-01-21 16:51:47 +0100

    no cli support

--- plugin.php
+++ plugin.php
@@ -6,3 +6,4 @@ $ilias_min_version = '6.0';
 $ilias_max_version = '7.9999';
 $responsible       = 'studer + raimann ag - Team Core 1';
 $responsible_mail  = 'support-core1@studer-raimann.ch';
+$supports_cli_setup = false;
